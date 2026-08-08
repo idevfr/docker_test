1bb465c2c9a4d73f3c826e979e534d3391ccf6b9
idevfr <utubediscow@gmail.com>
2026-08-09 01:05:40 +0600

    added route for product

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,6 @@ import express from "express";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+import productRoute from "./routes/product.route.js";
+app.use("/api/v1/product", productRoute);
 export default app;
