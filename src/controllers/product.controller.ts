5a2aea3d89c325e3d5c83560ba6b2ea4746d7e71
idevfr <utubediscow@gmail.com>
2026-08-09 23:21:57 +0600

    updated routes and controllers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -16,3 +16,24 @@ export const createProduct: RequestHandler = async function (req, res) {
     throw error;
   }
 };
+export const getAllProduct: RequestHandler = async function (req, res) {
+  try {
+    const products = await Product.find();
+    return res.status(200).json(products);
+  } catch (error) {
+    throw error;
+  }
+};
+export const deleteProduct: RequestHandler = async function (req, res) {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    if (!id) {
+      throw new Error("id is missing");
+    }
+    const deletedProduct = await Product.deleteOne({ _id: id });
+    return res.status(200).json({});
+  } catch (error) {
+    throw error;
+  }
+};
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { createProduct } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProduct,
+} from "../controllers/product.controller.js";
 const Router = express.Router();
 Router.route("/create").post(createProduct);
+Router.route("/get-all").get(getAllProduct);
+Router.route("/delete/:id").post(deleteProduct);
 export default Router;
